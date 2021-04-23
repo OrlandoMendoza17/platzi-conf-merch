1d1b3620cc5eac2d1e406a4464bfcf5af23ca20d
OrlandoMendoza17 <ommv.17@gmail.com>
2021-04-23 12:03:53 -0200

    Instalacion de babel y html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -12,13 +13,22 @@ module.exports = {
   resolve:{
     extensions: ['.js', '.jsx']
   },
-  // module:{
-  //   rules:[
-  //     {
-  //       test: /\.(js|jsx)/,
-  //       exclude: /node_modules/,
-  //       loader: 'babel-loader'
-  //     }
-  //   ]
-  // }
+  module:{
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      // favicon: ''
+      inject: 'body',
+      title: 'PlatziConf-Merch',
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html'
+    })
+  ]
 }
