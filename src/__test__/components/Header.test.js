a6602b1162026167daa8b364c00240a4e7c11d73
OrlandoMendoza17 <ommv.17@gmail.com>
2021-05-11 22:58:01 -0200

    Se añadió un Snapshot a Header.test

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import {create} from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Header from '../../widgets/Header';
 import ProviderMock from '../../__mocks__/ProviderMock';
 
 
 describe('<Header />', ()=>{
+  const Component = (
+    <ProviderMock>
+      <Header/>
+    </ProviderMock>
+  )
+  
   test('Render del Componente Header', () => {
-    const header = shallow(
-      <ProviderMock>
-        <Header/>
-      </ProviderMock>
-    )
-    expect(header.length).toEqual(1)
+    const header = shallow(Component);
+    expect(header.length).toEqual(1);
+  });
+  
+  test('Comprobar la UI de Header', () => {
+    const header = create(Component);
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
